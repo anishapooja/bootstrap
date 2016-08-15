152e228cd5fbcb159c0c99c569a14c111b8e6ddf
AP <anishapooja@outlook.com>
2016-08-15 14:35:17 +0530

    Add files via upload

---
+++ hhhf.js
@@ -0,0 +1,9 @@
+
+<script type="text/javascript">
+var poojaAp; //create or declare a variable by putting the keyword 
+var a;
+var b= 7;
+var a=b;
+console.log("javascript is working perfectly");
+</script>
+
